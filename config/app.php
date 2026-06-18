7a1a3576d7eab14a88b0fb5af607136b4729419f
Yuri Neves <yurineves92@gmail.com>
2026-06-18 07:13:33 -0300

    update

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'CRMLivewire'),
+    'name' => env('APP_NAME', 'CRM Livewire'),
 
     /*
     |--------------------------------------------------------------------------
